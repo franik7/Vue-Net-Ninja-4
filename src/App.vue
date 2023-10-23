e2a4a795984cb2552d7e04237f2241be5c7ac053
Frants <118231497+franik7@users.noreply.github.com>
2023-10-23 17:49:06 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+<h1>{{ title }}</h1>
+<div v-if="showModal">
+  <Modal :header = "header" :text = "text" theme="sale"/>
+</div>
+<button @click = "toggleModal">open modal</button>
+<!-- <input type = "text" ref="name">
+<button @click = "handleClick">click me</button> -->
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {Modal},
+  data() {
+    return {
+      title: 'My First Vue App :)',
+      header: "Sign up for the giveaway",
+      text: "Grab your ninja swag!",
+      showModal: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      
+    }
+  //   handleClick(){
+  //     console.log(this.$refs.name)
+  //     this.$refs.name.classList.add("active")
+  //     this.$refs.name.focus()
+  //   }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
